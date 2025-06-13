95b00552e397977fa2e89142524dc3c20964cb9e
The codeBro <twittercodebro@gmail.com>
2025-06-13 11:54:59 +0100

    added comments

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,11 @@
+//The below variables store the elements needed for mobile menu panel triggering on the page
 const menuBtn = document.querySelector(".menu-toggle-icon");
 const navlinks = document.querySelector(".navlinks");
 const copyright = document.querySelector(".copyright-year");
 const bodyElementTag = document.body;
 const mainElementTag = document.querySelector("main");
 
+//A function that takes html element as an argument, listen for a click event on the element to trigger navlinks active and inactive state on mobile and tablet devices
 function menuAndLinkToggle(toggleElement, affectedElement) {
   toggleElement.addEventListener("click", () => {
     if (!affectedElement.classList.contains("active")) {
@@ -19,12 +21,14 @@ function menuAndLinkToggle(toggleElement, affectedElement) {
 }
 menuAndLinkToggle(menuBtn, navlinks);
 
+//An iterated event listener that toggles in-ativity of the menu panel when active on mobile and tab devices
 const eachLinks = document
   .querySelectorAll(".navlinks li a")
   .forEach((link) => {
     menuAndLinkToggle(link, navlinks);
   });
 
+//JS date object used to get cuurent year for footer copyright year
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 copyright.innerText = `©${year}`;
