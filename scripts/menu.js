2f4f9f662f06a373e0bcd3033774fc03b7f5a7a7
The codeBro <twittercodebro@gmail.com>
2025-07-24 10:48:58 +0100

    cleaned up code

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,35 +2,28 @@
 const menuBtn = document.querySelector(".menu-toggle-icon");
 const navlinks = document.querySelector(".navlinks");
 const copyright = document.querySelector(".copyright-year");
-const bodyElementTag = document.body;
-const mainElementTag = document.querySelector("main");
 
-//A function that takes html element as an argument, listen for a click event on the element to trigger navlinks active and inactive state on mobile and tablet devices
-function menuAndLinkToggle(toggleElement, affectedElement) {
-  toggleElement.addEventListener("click", () => {
-    if (!affectedElement.classList.contains("active")) {
-      affectedElement.classList.add("active");
-      menuBtn.src = "icons/close-svgrepo-new.svg";
-      bodyElementTag.classList.add("no-scroll");
-    } else {
-      affectedElement.classList.remove("active");
-      menuBtn.src = "icons/menu (2).svg";
-      bodyElementTag.classList.remove("no-scroll");
-    }
-  });
+//An event listener for a click event on the menu btn element to trigger navlinks active and inactive state on mobile and tablet devices
+function menuToggle(eventElement, affectedElement) {
+    eventElement.addEventListener("click", () => {
+        affectedElement.classList.toggle("active");
+        if (affectedElement.classList.contains("active")) {
+            eventElement.src = "icons/close-svgrepo-new.svg";
+        } else {
+            eventElement.src = "icons/menu (2).svg";
+        }
+    });
 }
-menuAndLinkToggle(menuBtn, navlinks);
+menuToggle(menuBtn, navlinks);
 
 //An iterated event listener that toggles in-ativity of the menu panel when active on mobile and tab devices
 const eachLinks = document
-  .querySelectorAll(".navlinks li a")
-  .forEach((link) => {
-    menuAndLinkToggle(link, navlinks);
-  });
+    .querySelectorAll(".navlinks li a")
+    .forEach((link) => {
+        menuToggle(link, navlinks);
+    });
 
 //JS date object used to get cuurent year for footer copyright year
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 copyright.innerText = `${year}`;
-console.log(copyright.innerText);
-console.log(copyright);
