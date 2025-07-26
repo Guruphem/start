0a75cf6644938bdf66a5e4ba9746de3b0be71393
The codeBro <twittercodebro@gmail.com>
2025-07-26 21:19:59 +0100

    modified close icon src

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,23 +5,23 @@ const copyright = document.querySelector(".copyright-year");
 
 //An event listener for a click event on the menu btn element to trigger navlinks active and inactive state on mobile and tablet devices
 function menuToggle(eventElement, affectedElement) {
-    eventElement.addEventListener("click", () => {
-        affectedElement.classList.toggle("active");
-        if (affectedElement.classList.contains("active")) {
-            eventElement.src = "icons/close-svgrepo-new.svg";
-        } else {
-            eventElement.src = "icons/menu (2).svg";
-        }
-    });
+  eventElement.addEventListener("click", () => {
+    affectedElement.classList.toggle("active");
+    if (affectedElement.classList.contains("active")) {
+      eventElement.src = "icons/close.png";
+    } else {
+      eventElement.src = "icons/menu (2).svg";
+    }
+  });
 }
 menuToggle(menuBtn, navlinks);
 
 //An iterated event listener that toggles in-ativity of the menu panel when active on mobile and tab devices
 const eachLinks = document
-    .querySelectorAll(".navlinks li a")
-    .forEach((link) => {
-        menuToggle(link, navlinks);
-    });
+  .querySelectorAll(".navlinks li a")
+  .forEach((link) => {
+    menuToggle(link, navlinks);
+  });
 
 //JS date object used to get cuurent year for footer copyright year
 const currentDate = new Date();
