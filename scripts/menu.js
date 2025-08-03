88c8e8bff910b75e0b0c882d709ec6caa58df8c7
The codeBro <twittercodebro@gmail.com>
2025-08-03 07:37:07 +0100

    refractor the code

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,6 +3,7 @@ const menuBtn = document.querySelector(".menu-toggle-icon");
 const navlinks = document.querySelector(".navlinks");
 const copyright = document.querySelector(".copyright-year");
 
+<<<<<<< HEAD
 //An event listener for a click event on the menu btn element to trigger navlinks active and inactive state on mobile and tablet devices
 function menuToggle(eventElement, affectedElement) {
   eventElement.addEventListener("click", () => {
@@ -15,6 +16,22 @@ function menuToggle(eventElement, affectedElement) {
   });
 }
 menuToggle(menuBtn, navlinks);
+=======
+function menuAndLinkToggle(toggleElement, affectedElement) {
+  toggleElement.addEventListener("click", () => {
+    if (!affectedElement.classList.contains("active")) {
+      affectedElement.classList.add("active");
+      menuBtn.src = "icons/close-svgrepo-new.svg";
+      bodyElementTag.classList.add("no-scroll");
+    } else {
+      affectedElement.classList.remove("active");
+      menuBtn.src = "icons/menu (2).svg";
+      bodyElementTag.classList.remove("no-scroll");
+    }
+  });
+}
+menuAndLinkToggle(menuBtn, navlinks);
+>>>>>>> 8d715ae (replaced icon and fixed mobile menu errrors)
 
 //An iterated event listener that toggles in-ativity of the menu panel when active on mobile and tab devices
 const eachLinks = document
