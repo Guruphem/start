8248b909ea452915179dee72d42dffb267c05868
The codeBro <twittercodebro@gmail.com>
2025-08-03 20:48:42 +0100

    changed mobile menu btn icon to an animated icon

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,34 +6,34 @@ const wrapper = document.querySelector("nav");
 
 //An event listener for a click event on the menu btn element to trigger navlinks active and inactive state on mobile and tablet devices
 function menuToggle(eventElement, affectedElement) {
-  eventElement.addEventListener("click", () => {
-    affectedElement.classList.toggle("active");
-    if (affectedElement.classList.contains("active")) {
-      eventElement.src = "icons/close.png";
-      document.body.classList.add("no-scroll");
-    } else {
-      eventElement.src = "icons/menu (2).svg";
-      document.body.classList.remove("no-scroll");
-    }
-  });
+    eventElement.addEventListener("click", () => {
+        affectedElement.classList.toggle("active");
+        if (affectedElement.classList.contains("active")) {
+            eventElement.classList.add("open");
+            document.body.classList.add("no-scroll");
+        } else {
+            eventElement.classList.remove("open");
+            document.body.classList.remove("no-scroll");
+        }
+    });
 }
 menuToggle(menuBtn, navlinks);
 
 const eachLinks = document
-  .querySelectorAll(".navlinks li a")
-  .forEach((link) => {
-    menuToggle(link, navlinks);
-    preventPageReload(link, navlinks);
-  });
+    .querySelectorAll(".navlinks li a")
+    .forEach((link) => {
+        menuToggle(link, navlinks);
+        preventPageReload(link, navlinks);
+    });
 
 //A function that prevents the browser auto reload when a navlink of the current page is clicked
 function preventPageReload(link) {
-  link.addEventListener("click", (event) => {
-    if (link.href === window.location.href) {
-      event.preventDefault();
-      menuBtn.src = "icons/menu (2).svg";
-    }
-  });
+    link.addEventListener("click", (event) => {
+        if (link.href === window.location.href) {
+            event.preventDefault();
+            menuBtn.classList.remove("open");
+        }
+    });
 }
 
 //JS date object used to get cuurent year for footer copyright year
